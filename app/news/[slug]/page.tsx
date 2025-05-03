294e8b7714b30b626b60d0d21e9326e0a69f70c9
Augustika <augustika1524@gmail.com>
2025-05-03 16:31:50 +0530

    added intercepted image page

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { DUMMY_NEWS } from "@/dummy-news";
-import Image from "next/image";
+
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 
 type NewsDetailsPageProps = {
@@ -23,7 +24,10 @@ function NewsDetailsPage({ params }: NewsDetailsPageProps) {
 
     return (<article className="news-article">
         <header>
+            <Link href={`/news/${newsItem.slug}/image`}>
             <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            </Link>
+           
             <h1>{newsItem.title}</h1>
             <time dateTime={newsItem.date}>{newsItem.date}</time>
         </header>
