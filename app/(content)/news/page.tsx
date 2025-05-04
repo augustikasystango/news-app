6f58d68d1635d681407389e1ec9c9aa15293158f
Augustika <augustika1524@gmail.com>
2025-05-04 15:43:50 +0530

    data fetching-csr

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,56 @@
+'use client';
 import NewsList from "@/component/news-list"
-import { DUMMY_NEWS } from "@/dummy-news"
+import { useEffect,useState } from "react";
+// import { DUMMY_NEWS } from "@/dummy-news"
 
 function NewsPage() {
+
+  const [error,setError]= useState('');
+  const [isLoading , setIsLoading]=useState(false);
+  const [news,setNews]=useState();
+
+
+  useEffect(()=>{
+    const fetchNews=async()=>{
+      setIsLoading(true);
+       const res = await fetch(`http://localhost:8080/news`);
+      
+       if(!res.ok)
+       {
+         setError('Failed to load news.');
+         setIsLoading(false);
+        
+       }
+        const news = await res.json();
+        setIsLoading(false);
+        setNews(news);
+   
+      
+     }
+   fetchNews();
+  },[]);
+
+
+  if(isLoading)
+  {
+    return <p>Loading ...</p>
+  }
+  if(error)
+  {
+    return <p>Error</p>
+  }
+
+  let newsContent ;
+  if(news)
+  {
+    newsContent = <NewsList news={news}/>
+  }
+
   return (
     <>
     <h2>News Page</h2>
-   <NewsList news={DUMMY_NEWS}/>
+    {newsContent}
+  
    
     </>
   )
